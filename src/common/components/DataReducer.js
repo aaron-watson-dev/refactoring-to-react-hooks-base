288b3dd7d0680b995546916e55e8bd5f921062a8
aaron-watson-dev <1732699+aaron-watson-dev@users.noreply.github.com>
2021-11-15 22:12:45 -0500

    woof

--- src/common/components/DataReducer.js
+++ src/common/components/DataReducer.js
@@ -1,24 +1,27 @@
 /* jshint esversion: 6 */
-import * as Constants from "../constants"
+import * as Constants from "../constants";
 
 export default function DataReducer (state, action) {
    switch(action.type)  {
-       case "SET-RESPONSE":
+       case "SET-RESPONSE": {
            return {
                ...state,
                currentData: action.payload,
                currentState: Constants.DATA_FETCHING_STATUS.LOADED
-           };
-       case "ERROR":
+           }
+       }
+       case "ERROR": {
            return {
                ...state,
                currentState: Constants.DATA_FETCHING_STATUS.ERROR
            };
-       case "LOADING":
+       }
+       case "LOADING": {
            return {
                ...state,
                currentState: Constants.DATA_FETCHING_STATUS.LOADING
            };
+      }
       default:
            return state;
    }
